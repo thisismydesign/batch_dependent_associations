650f7898f37b65d656cc79c49798d53b4e50addb
Csaba Apagyi <csaba.apagyi@gmail.com>
2018-04-09 18:12:08 +0200

    Fixes alignments

--- spec/example_project/models/safe_person.rb
+++ spec/example_project/models/safe_person.rb
@@ -1,6 +1,5 @@
 class SafePerson < ActiveRecord::Base
-    include BatchDependentAssociations
+  include BatchDependentAssociations
 
-    has_many :bank_accounts, foreign_key: :person_id, class_name: "BankAccount", dependent: :destroy
-  end
-  
+  has_many :bank_accounts, foreign_key: :person_id, class_name: "BankAccount", dependent: :destroy
+end
